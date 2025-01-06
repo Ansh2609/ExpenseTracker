7822d4fa5bdbf169a28b69d10efbb3301eb089e3
Ansh2609 <73031691+Ansh2609@users.noreply.github.com>
2025-01-06 18:59:46 -0500

    Add files via upload
    The individual classes for objects concerned with Expenditure including expenses and their category, also has helper methods to retrieve the value of total expenses

---
+++ Expenditure.java
@@ -0,0 +1,26 @@
+public class Expenditure
+
+{
+    String category ; 
+    double expense ; 
+
+public Expenditure(String category, double expense)
+
+{
+    this.category = category ; 
+    this.expense = expense ; 
+}
+
+public String getCategory(String category)
+
+{
+    return category ; 
+}
+public double getExpense(double expense) 
+
+{
+    return expense ; 
+}
+
+
+}
