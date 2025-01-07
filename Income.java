7016460178dd36edd2edb918bc189bb8b41da2ce
Ansh2609 <73031691+Ansh2609@users.noreply.github.com>
2025-01-06 19:00:29 -0500

    Add files via upload
    Similar to the previous file, contains the initialization of classes and objects concerned with income, also has helper methods.

---
+++ Income.java
@@ -0,0 +1,22 @@
+public class Income 
+{
+    String source ; 
+    double amount ; 
+
+
+public Income(String source, double amount) 
+{
+    this.source = source ; 
+    this.amount = amount ; 
+}
+public String getSource(String source) 
+{
+    return source ; 
+}
+
+public double getAmount(double amount)
+{
+    return amount ; 
+}
+
+}
